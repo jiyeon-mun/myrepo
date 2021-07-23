b0d3806a7ceaf0ce0502038f8ba2394d54585220
wldus <wldus@LAPTOP-ER5SKS2U>
2021-07-23 13:53:39 +0900

    처음으로 커밋하는 코드

---
+++ project1/src/com/kh/exam2/PracPrint.java
@@ -0,0 +1,14 @@
+package com.kh.exam2;
+
+public class PracPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("안녕하세요!! 반갑습니다.");
+		System.out.println();
+		System.out.println("안녕하세요");
+		System.out.print("제 이름은 홍길동 입니다.");
+	}
+
+}
---
+++ project1/src/com/kh/exam3/EscapeChar.java
@@ -0,0 +1,23 @@
+package com.kh.exam3;
+
+public class EscapeChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * 이스케이프 문자
+		 * 	\(역슬래쉬) 문자와의 조합으로 만들어진다.
+		 * 	\n : 문자열 안에서 개행 기능
+		 * 	\t : 문자열 안에서 탭 공백(8칸 공백)
+		 * 	\" : 문자열 안에서 쌍따옴표
+		 * 	\' : 문자열 안에서 홀따옴표
+		 * 	\\ : 문자열 안에서 역슬래쉬
+		 * 	\r : 문자열 안에서 커서의 위치를 해당 줄의 앞으로 이동
+		 	ex) abcd\r12; 12cd
+		*/
+		
+		System.out.println("C:\\Program Files\\AdoptOpenJDK");
+	}
+
+}
