c16ca90edcd239039d7b0c6041b4337cf08e8f86
jiyeon-mun <wldusm5398@naver.com>
2021-08-02 15:28:23 +0900

    배열 복사 문제들

--- project1/src/com/kh/exam8/ArrayData.java
+++ project1/src/com/kh/exam8/ArrayData.java
@@ -1,7 +1,12 @@
 package com.kh.exam8;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayData {
 
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void sample1() {
 		int[] iArr = new int[5];
 
@@ -33,11 +38,130 @@ public class ArrayData {
 		}
 	}
 
-
+	public static void sample6() {
+		int[] iArr1= new int[] {1,2,3,4};
+		
+		System.out.println("-----배열 복사(얕은 복사)-----");
+		
+		int[] iArr2=iArr1;
+		for (int i = 0; i < iArr1.length; i++) {
+			System.out.println("iArr1[" + i + "]:" + iArr1[i] + "|iArr2[" + i + "]:" + iArr2[i]);
+		}
+		
+		System.out.println("-----배열 복사 후 값 변경-----");
+		
+		// 동일한 참조값으로 변경하기 때문에 iArr1, iArr2 모두 변경됨
+		iArr1[0]=15; // iArr2 배열의 값도 변경됨
+		iArr2[3]=25; // iArr1 배열의 값도 변경됨
+		for (int i = 0; i < iArr1.length; i++) {
+			System.out.println("iArr1[" + i + "]:" + iArr1[i] + "|iArr2[" + i + "]:" + iArr2[i]);
+		}
+		
+		System.out.println("-----참조값 확인-----");
+		// 동일한 참조값을 갖는다. 그렇기때문에 각 배열 값 변경 시 다른 배열도 함께 변경된다.
+		System.out.println("iArr1 참조값 : "+iArr1); // 출력 시 나오는 값은 실제 주소가 아님.
+		System.out.println("iArr2 참조값 : "+iArr2);
+	}
+	
+	public static void sample7() {
+		int[] iArr1 = new int[] { 1, 2, 3, 4 };
+		int[] iArr2 = new int[iArr1.length];
+		
+		System.out.println("-----배열 복사(깊은 복사)-----");
+		for(int i=0;i<iArr1.length;i++) {
+			iArr2[i]=iArr1[i]; // 값만 복사되며, 참조값은 서로 다름.
+		}
+		for(int i=0;i<iArr1.length;i++) {
+			System.out.println("iArr1[" + i + "]:" + iArr1[i] + "|iArr2[" + i + "]:" + iArr2[i]);
+		}
+		
+		System.out.println("-----배열 복사 후 값 변경-----");
+		// 원하는 배열에 대해 원하는 요소의 값만 변경됨. 참조값이 서로 다르기 때문에
+		iArr1[0]=15; 
+		iArr2[3]=25;
+		for(int i=0;i<iArr1.length;i++) {
+			System.out.println("iArr1[" + i + "]:" + iArr1[i] + "|iArr2[" + i + "]:" + iArr2[i]);
+		}
+		
+		System.out.println("-----참조값 확인-----");
+		System.out.println("iArr1 참조값 : "+iArr1);
+		System.out.println("iArr2 참조값 : "+iArr2);
+	}
+	
+	public static void sample8() {
+		// 깊은 복사를 사용하여 배열의 크기 늘리기.
+		int[] iArr1 = new int[] { 1, 2, 3, 4 };
+		
+		System.out.println("-----배열 크기 늘리기 전-----");
+		for(int i=0;i<iArr1.length;i++) {
+			System.out.println("iArr1[" + i + "]:" + iArr1[i]);
+		}
+		
+		System.out.println();
+		
+		int[] iArr2 = new int[iArr1.length+1]; // iArr2 배열의 크기는 iArr1 배열의 크기+1
+		for(int i=0;i<iArr1.length;i++) {
+			iArr2[i]=iArr1[i]; 
+			//System.out.println("iArr2[" + i + "]:" + iArr2[i]);
+		}
+		//System.out.println(Arrays.toString(iArr2)); // [1,2,3,4,0]
+		
+		System.out.println();
+		
+		iArr1=iArr2; // 얕은 복사 활용하여 배열 크기 늘리기.
+		// System.out.println(Arrays.toString(iArr1)); // [1,2,3,4,0]
+		
+		iArr1[iArr2.length-1]=5; // 마지막 index(iArr[3])에 값 5 저장
+		
+		System.out.println("-----배열 크기 늘린 후-----");
+		for(int i=0;i<iArr1.length;i++) {
+			System.out.println("iArr1[" + i + "]:" + iArr1[i]);
+		}
+	}
+	
+	public static void sample10() {
+		int num;
+		int[] iArr = new int[0];
+		int sum=0;
+		
+		while(true) {
+			System.out.println("1 ~ 49 사이의 정수 값을 입력해주세요.(-1 입력하면 종료)");
+			System.out.print(": ");
+			num = sc.nextInt(); sc.nextLine();
+			
+			if(num!=-1) {
+				int[] copyNum = new int[iArr.length + 1];
+				
+				// 새로운 배열에 기존 배열 값 복사
+				System.arraycopy(iArr,	 0, 		copyNum, 0, 		iArr.length); 
+							// 원본배열명, 원본복사위치, 복사본배열명, 복사본복사위치, 복사할길이(원본배열길이)
+//				for(int i=0;i<iArr.length;i++) {
+//					copyNum[i]=iArr[i]; // 값 복사
+//				}
+				iArr=copyNum;
+				System.out.println("copyNum[]: " + Arrays.toString(copyNum));
+				// 새로운 배열에 기존 배열의 데이터 값들이 복사. 추가된 +1의 자리엔 0값으로 초기화됨.
+				
+				iArr[iArr.length-1]=num;
+				sum+=num; // 입력값들 반복해서 덧셈
+				System.out.println("iArr[]: " + Arrays.toString(iArr)); // 입력값들 배열 내 마지막 index에 저장됨.
+			}
+			else {
+				break;
+			}
+		}
+		System.out.println();
+		System.out.println("입력한 정수들의 합은 : " + sum);
+	}
+	
 	public static void main(String[] args) {
 		//sample1();
 		//sample2();
-		sample3();
+		//sample3();
+		//sample6();
+		//sample7();
+		//sample8();
+		sample10();
 	}
 
 }
--- project1/src/com/kh/exam8/PracArray.java
+++ project1/src/com/kh/exam8/PracArray.java
@@ -108,7 +108,85 @@ public class PracArray {
 		
 	}
 
+	// 가위, 바위, 보 랜덤 출력_ 강사님 방식: 승률 추가
 	public static void ex8() {
+		String player;
+		String computer = "";
+		int[] score = new int[3];	// 0: 승, 1: 패, 2: 무
+		boolean exitFlag = false;
+		
+		while(true) { // 게임 무한반복
+		
+			while(true) { // 사용자 입력의 무한반복으로 "종료" 입력 시 종료 조건을 통해 게임의 무한반복이 종료된다.
+				System.out.print("가위, 바위, 보 중 하나 입력 : ");
+				player = sc.nextLine();
+				if(player.equals("가위") || player.equals("바위") || player.equals("보")
+						|| player.equals("종료")) { // 사용자 입력
+					if (player.equals("종료")) {exitFlag = true;} // 종료 입력 시
+					
+					break; // 사용자 입력 하면 입력문이 끝나고 switch문으로 넘어간다.
+				}
+			}
+			
+			if (exitFlag) { // 무한 반복 종료
+				break;
+			}
+			
+			switch(rd.nextInt(3)) {
+				case 0:
+					computer = "가위";	break;
+				case 1:
+					computer = "바위";	break;
+				case 2:
+					computer = "보";		break;
+			}
+			
+			if(player.equals("가위")) {
+				if(computer.equals("바위")) {
+					System.out.println("컴퓨터 승");
+					score[1] += 1;
+				} else if(computer.equals("보")) {
+					System.out.println("플레이어 승");
+					score[0] += 1;
+				} else if(computer.equals("가위")) {
+					System.out.println("무승부");
+					score[2] += 1;
+				}
+				
+			} else if(player.equals("바위")) {
+				if(computer.equals("바위")) {
+					System.out.println("무승부");
+					score[2] += 1;
+				} else if(computer.equals("보")) {
+					System.out.println("컴퓨터 승");
+					score[1] += 1;
+				} else if(computer.equals("가위")) {
+					System.out.println("플레이어 승");
+					score[0] += 1;
+				}
+				
+			} else if(player.equals("보")) {
+				if(computer.equals("바위")) {
+					System.out.println("플레이어 승");
+					score[0] += 1;
+				} else if(computer.equals("보")) {
+					System.out.println("무승부");
+					score[2] += 1;
+				} else if(computer.equals("가위")) {
+					System.out.println("컴퓨터 승");
+					score[1] += 1;
+				}
+				
+			}
+			
+			System.out.println("현재 플레이어의 스코어는 : "+score[0]+" 승 "+score[1]+" 패 "+score[2]+" 무 입니다.");
+		} // 게임 무한반복의 while문 괄호
+		
+		System.out.println(Arrays.toString(score)); // 사용자 승,패,무 저장한 배열 출력
+	}
+	
+	// 가위, 바위, 보 랜덤 출력 내 방식_ 배열이용
+	public static void ex10() {
 		String[] game= {"가위","바위","보"}; // 사용자 입력과 비교하는 컴퓨터 랜덤을 배열로 저장
 		String text; // 사용자 입력
 		
@@ -179,20 +257,118 @@ public class PracArray {
 		}
 	}
 	
-	public static void ex10() {}
 	
-	public static void ex11() {}
+	public static void ex11() {
+		// 사용자 입력을 통해 과목 정보를 입력 받는다.
+		// 사용자 입력이 계속 이루어 질 때마다 배열의 크기를 +1씩 늘려 입력한 만큼 저장하게 한다.
+		// 사용자 입력이 "종료"인 경우 더 이상 입력을 받지 않고 최종 입력된 모든 과목 정보를 출력.
+		/*
+		 *  1. 배열의 크기가 0 인 문자열 배열을 생성
+		 *  2. 사용자 입력을 계속 받을 수 있도록 무한 반복문 작성
+		 *  3. 무한 반복문 안에서 사용자 입력을 받도록 작성
+		 *  4. 사용자 입력값이 종료가 아니면 기존 배열 크기 +1을 한 새로운 배열 생성
+		 *  5. 새로 생성한 배열에는 기존 배열의 데이터를 깊을 복사로 저장
+		 *  6. 복사본의 참조 주소를 기존 배열 변수에서 사용할 참조 주소로 저장(얕은 복사)
+		 *  7. 4번에서 입력받은 데이터를 새로 추가된 배열의 마지막 인덱스에 저장
+		 *  8. 4번에서 입력받은 데이터가 종료인 경우 무한 반복문을 종료할 수 있게 한다.
+		 *  9. 8번에서 최종 무한 반복이 종료되면 배열에 저장된 데이터를 출력.
+		 */
+		
+		String info=""; // 과목명 입력
+		String[] sub = new String[0]; // 과목명 저장할 배열 선언 및 초기화. 아직 입력하지 않았으니 0으로 초기화
+		
+		while(true) { // 무한반복_ "종료" 입력 전까지 과목명 무한으로 입력하도록
+			System.out.print("과목명 입력 : ");
+			info=sc.nextLine();
+			if(!info.equals("종료")) { // 과목명 데이터 입력에 대해서
+				
+				// 배열 크기를 늘려서 깊은 복사를 하기 위한 영역
+				String[] copySub = new String[sub.length + 1]; // 기존 배열 크기 +1을 한 새로운 문자열 배열 생성. 
+				// System.out.println(Arrays.toString(copySub)); //계속해서 기존 배열에 +1한 배열크기의 새로운 배열이 생성된다!(초기화는 null값)
+				
+				for(int i=0;i<sub.length;i++) {
+					copySub[i]=sub[i]; // 새로운 배열에 기존 배열의 데이터 값을 깊은 복사. 데이터 값만 복사한다.
+				}
+				sub=copySub; // sub배열과 copySub 배열의 참조값과 동일(얕은 복사 이용).
+				sub[sub.length-1]=info; // 입력한 데이터를 기존 배열의 마지막 index에 저장.
+			} 
+			else { // "종료" 입력하면 입력 종료 및 무한반복 종료
+				break;
+			}
+		}
+		for(int i=0;i<sub.length;i++) { // 입력한 과목명을 저장한 배열에 대한 데이터 출력 및 확인
+			System.out.println("sub[" + i + "]:" + sub[i]);
+		}
+		System.out.println(Arrays.toString(sub));
+	}
+		
+	public static void ex12() {
+		int num;
+		int[] iArr = new int[0];
+		int sum=0;
+		
+		while(true) {
+			System.out.println("1 ~ 49 사이의 정수 값을 입력해주세요.(-1 입력하면 종료)");
+			System.out.print(": ");
+			num = sc.nextInt(); sc.nextLine();
+			
+			if(num!=-1) {
+				int[] copyNum = new int[iArr.length + 1];
+				
+				for(int i=0;i<iArr.length;i++) {
+					copyNum[i]=iArr[i]; // 값 복사
+				}
+				iArr=copyNum;
+				iArr[iArr.length-1]=num;
+				sum+=num; // 입력값들 반복해서 덧셈
+				// System.out.println("iArr[]: " + Arrays.toString(iArr)); // 입력값들 배열 내 마지막 index에 저장됨.
+			}
+			else {
+				break;
+			}
+		}
+		System.out.println();
+		System.out.println("최종 iArr[]: " + Arrays.toString(iArr));
+		System.out.println("입력한 정수들의 합은 : " + sum);
+	}
+	
+	public static void ex13() {
+		String text;
+		String[] msgArr = new String[0];
+		
+		System.out.println("영문자 문자열을 입력해주세요.");
+		System.out.print(": ");
+		text = sc.nextLine();
+		
+		
+		
+	}
+	
+	public static void ex() {
+		String[] sArr = new String[] { "국어", "수학", "영어", "과학", "사회" };
+		String subject = "";
+		
+		System.out.println("배열 출력 : "+Arrays.toString(sArr));
+		
+		System.out.print("제거할 과목명을 입력해주세요 : ");
+		subject=sc.nextLine();
+	}
 	
 	public static void main(String[] args) {
 		//ex1();
 		//ex2();
 		//ex3();
-		ex4();
+		//ex4();
 		//ex5();
 		//ex6();
 		//ex7();
 		//ex8();
+		//ex10();
 		//ex9();
+		
+		//ex11();
+		ex12();
+		
 		
 
 	}
