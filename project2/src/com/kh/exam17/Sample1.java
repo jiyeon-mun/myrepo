b54901ca8ed36320f4e113e07c51d3b00f9d37b4
jiyeon-mun <wldusm5398@naver.com>
2021-08-21 15:31:21 +0900

    0821 컬렉션 ArrayList 추가문제 8번까지

--- project2/src/com/kh/exam17/Sample1.java
+++ project2/src/com/kh/exam17/Sample1.java
@@ -1,11 +1,14 @@
 package com.kh.exam17;
 
 import java.util.*;
-//import java.util.ArrayList; import java.util.List; import java.util.Random;
+// import java.util.ArrayList; import java.util.List; import java.util.Random;
+// import java.util.Date;
 
 public class Sample1 {
 	public static Scanner sc = new Scanner(System.in);
 	public static Random rd = new Random();
+	public static Date date1 = new Date();
+	public static Calendar date2 = Calendar.getInstance();
 	
 	public static void ex1() {
 		List<Integer> aList = new ArrayList<Integer>(); // 다형성 접목
@@ -62,6 +65,11 @@ public class Sample1 {
 	}
 
 	public static void ex2() {
+		/*
+		 * 사용자 입력으로 정수 데이터를 받아 ArrayList 에 저장 후
+		 * ArrayList에 저장된 모든 정수의 합을 구하여 출력한다.
+		 * (단, 사용자 입력은 한 번만 사용한다.)
+		 */
 		System.out.println("공백을 구분자로 사용하여 다음 내용에 맞은 데이터를 입력하세요.");
 		System.out.print("정수값을 2개 이상 입력하세요. : ");
 		String[] inputs=sc.nextLine().split(" ");
@@ -91,6 +99,11 @@ public class Sample1 {
 	}
 	
 	public static void ex3() {
+		/*
+		 * 사용자 입력으로 실수 데이터를 받아 ArrayList 에 저장 후
+		 * ArrayList에 저장된 모든 실수의 합과 평균을 구하여 출력한다.
+		 * (단, 사용자 입력은 한 번만 사용한다.)
+		 */
 		System.out.println("공백을 구분자로 사용하여 다음 내용에 맞은 데이터를 입력하세요.");
 		System.out.print("실수값을 2개 이상 입력하세요. : ");
 		String[] inputs=sc.nextLine().split(" ");
@@ -115,6 +128,12 @@ public class Sample1 {
 	}
 	
 	public static void ex4() {
+		/*
+		 * 다음의 요구사항을 잘 보고 요구사항에 맞도록 구현한다.
+		 * 	- 사용자 입력으로 정수 데이터를 받아 ArrayList 에 저장을 해야 한다.
+		 * 	- 사용자 입력으로 하나의 정수 데이터를 입력 할 수 있지만 2개 이상의 정수를 입력 하기도 한다.
+		 * 	- 동일한 정수 데이터는 ArrayList에 저장되지 않도록 한다.
+		 */
 		System.out.println("공백을 구분자로 사용하여 다음 내용에 맞은 데이터를 입력하세요.");
 		System.out.print("정수값을 한 개 이상 입력하세요. : ");
 		
@@ -198,20 +217,140 @@ public class Sample1 {
 		
 	}
 	
-	public static void ex6() {}
+	public static void ex6() {
+		/*
+		 * 현재 날짜를 구한 후 ArrayList에 년, 월, 일, 시간, 분, 초 데이터가
+		 * 저장되도록 한다.
+		 */
+		System.out.println("현재 날짜 출력: "+date1); // Sat Aug 21 12:36:35 KST 2021
+		
+		List<String> strList = new ArrayList<String>();
+		
+		String sdate1 = "%tY년";
+		strList.add(String.format(sdate1, date1));
+		
+		sdate1 = "%tm월";
+		strList.add(String.format(sdate1, date1));
+		
+		sdate1 = "%td일";
+		strList.add(String.format(sdate1, date1));
+		
+		sdate1 = "%tH시";
+		strList.add(String.format(sdate1, date1));
+		
+		sdate1 = "%tM분";
+		strList.add(String.format(sdate1, date1));
+		
+		sdate1 = "%tS초";
+		strList.add(String.format(sdate1, date1));
+		System.out.println(strList);
+	}
 	
-	public static void ex7() {}
+	public static void ex7() {
+		/*
+		 * 3초에 한번씩 정기적으로 1 ~ 99 사이의 랜덤값을 ArrayList 에 저장을 하도록 한다
+		 * ArrayList 에 저장하는 데이터는 10개로 제한한다.
+		 */
+		List<Integer> intList = new ArrayList<Integer>();
+		
+		System.out.println(date2.getTime()); // 현재 시간 출력 확인
+		
+		// Thread클래스의 sleep()메서드
+		// 		실행 중인 스레드 일정 시간 멈추게 만듦. 밀리세컨드 단위 사용.
+		//		try-catch문으로 감싸주고, catch 블록에서 잡힌 InterruptedException은 무시.
+		for(int i=0;i<10;i++) {
+			try {
+				int elements = rd.nextInt(99) + 1; // 1 ~ 99 사이의 랜덤값
+				
+				Thread.sleep(3000); // 3초간 일시정시 후 재실행
+				date2.add(Calendar.SECOND, 3); // 멈춰진 시간에 대해 3초씩 추가
+				String msg = date2.get(Calendar.HOUR)+":"+date2.get(Calendar.MINUTE)+":"+date2.get(Calendar.SECOND);
+				
+				System.out.print((i+1)+"회: "+elements+" | ");
+				System.out.println(msg); // 3초 후에 출력되는지 확인
+				intList.add(elements);
+				
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(intList);
+	}
 	
-	public static void ex8() {}
+	public static void ex8() {
+		/*
+		 * 3초에 한번씩 정기적으로 1 ~ 99 사이의 랜덤값을 ArrayList 에 저장을 할 때 저장 시간도
+		 * 같이 ArrayList에 저장하도록 하며 저장하는 데이터는 10개로 제한한다.
+		 * (단, 중복 데이터는 저장이 안되도록 하며, ArrayList를 2개 이용하도록 한다. 시간은 년월일
+		 * 시분초까지만 저장하도록 한다.)
+		 */
+		List<Integer> intList = new ArrayList<Integer>(); // 랜덤값 저장
+		List<String> strList = new ArrayList<String>(); // 시간 저장
+		System.out.println(date2.getTime()); // 시작시간 확인; 현재시간
+		
+		for(int i=0;i<10;i++) {
+			try {
+				int elements = rd.nextInt(99) + 1;
+				// 생성된 랜덤값이 랜덤값 저장 정수 컬렉션에 존재하지 않는 신규값인 경우
+				// 3초에 정기적으로 생성되는 랜덤값을 ArrayList 에 저장한다.
+				if(!intList.contains(elements)) {
+					Thread.sleep(3000);
+					date2.add(Calendar.SECOND, 3);
+					
+					int year = date2.get(Calendar.YEAR); // 년
+					int month = date2.get(Calendar.MONTH) + 1; // MONTH는 0부터 시작됨. 월
+					int day = date2.get(Calendar.DATE); // 일
+					int hour = date2.get(Calendar.HOUR); // 시
+					int min = date2.get(Calendar.MINUTE); // 분
+					int scd = date2.get(Calendar.SECOND); //초
+					String msg = year+"년 "+month+"월 "+day+"일 "+hour+":"+min+":"+scd;
+					
+					System.out.print((i+1)+"회: "+elements+" | ");
+					System.out.println(msg);
+					
+					intList.add(elements);
+					strList.add(msg);
+				}
+				// 생성된 랜덤값이 랜덤값 저장 컬렉션 내 이미 존재하는 경우
+				// 현재 인덱스에서 랜덤값을 재생성 한다.
+				else {
+					--i;
+					System.out.println("Duplicate!!"); // 중복여부 확인
+				}
+				
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		// 출력
+		System.out.println(intList);
+		System.out.println(strList);
+		System.out.println(date2.getTime()); // 종료시간 확인; 현재시간
+	}
 	
-	public static void ex9() {}
+	public static void ex9() {
+		/*
+		 * ArrayList 에 다음의 단어를 미리 초기화 한 후 다음 요구사항에 맞는 프로그램을 작성한다.
+		 * 	- abstract, continue, for, new, switch, default, package,
+		 * 	  synchronized, boolean, do, if, private, this, break,
+		 * 	  double, implements, protected, throw, byte, else, import,
+		 * 	  public, throws, case, instanceof, return, catch, extends,
+		 * 	  int, short, try, char, final, interface, static, void,
+		 * 	  class, finally, long, float, super, while
+		 * 	- ArrayList에 저장된 문자열을 랜덤으로 출력하게 만든다.
+		 * 	- 랜덤으로 출력된 문자열을 사용자 입력으로 사용자가 입력하게 만든다.
+		 * 	- 사용자가 입력한 데이터가 출력된 문자열과 동일하면 다음 문자열이 다시 랜덤으로 출력되게 한다.
+		 * 	- 위 과정을 10번 반복하여 모든 문자열을 입력한 총 시간과 정확하게 입력한 문자열의 수, 잘못
+		 * 	  입력한 문자열의 수를 테이블 형태로 출력하게 한다. 
+		 */
+	}
 	
 	public static void main(String[] args) {
 		ex9();
-		ex8();
-		ex7();
-		ex6();
-		ex5();
+		//ex8();
+		//ex7();
+		//ex6();
+		//ex5();
 		//ex4();
 		//ex3();
 		//ex2();
